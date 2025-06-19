709738f81ebcf20125add848b92d18a2e67a847c
Sharany25 <115199969+Sharany25@users.noreply.github.com>
2025-06-19 12:10:55 -0600

    Update SuccessToast.tsx

--- src/components/SuccessToast.tsx
+++ src/components/SuccessToast.tsx
@@ -1,4 +1,3 @@
-// components/SuccessToast.tsx
 import React, { useEffect, useRef } from "react";
 import { View, Text, StyleSheet, Animated, Dimensions } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
