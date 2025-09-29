86046e5951edfab457296e6c9e4daa558480d0e3
Sharany25 <osoriodiego877@gmail.com>
2025-09-29 12:29:17 -0600

    P3 Producciòn

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,8 +1,8 @@
 //APi local
-export const API_URL = "http://192.168.1.13:3000/MyFitGuide/";
+//export const API_URL = "http://192.168.1.13:3000/MyFitGuide/";
 
 //APi produccion en una EC2 en AWS
 //export const API_URL = "http://18.217.130.12:3000/MyFitGuide/";
 
 //APi produccion en Railway
-//export const API_URL = "https://myfitguideapi-production.up.railway.app/MyFitGuide/";
+export const API_URL = "https://myfitguideapi-production.up.railway.app/MyFitGuide/";
